488b9b3e99067370dd67aad8409fc1184c0c737e
Maheshwari-Khelge <145866806+Maheshwari-Khelge@users.noreply.github.com>
2023-11-19 12:22:04 +0530

    Add files via upload

---
+++ Modal-react-Assignment/steps/src/components/editModal.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+ 
+const EditModal = ({ isShow, onClose, children }) => {
+    if (!isShow) return null;
+ 
+    return (
+        <div
+            onClick={onClose}
+            style={{
+                position: "fixed",
+                top: 0,
+                left: 0,
+                width: "100%",
+                height: "100%",
+                background: "rgba(0, 0, 0, 0.5)",
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
+            }}
+        >
+            <div
+                style={{
+                    background: "white",
+                    height: 400,
+                    width: 300,
+                    margin: "auto",
+                    padding: "2%",
+                    border: "2px solid #000",
+                    borderRadius: "10px",
+                    boxShadow: "2px solid black",
+                }}
+            >
+                
+                {children}
+            </div>
+        </div>
+    );
+};
+ 
+export default EditModal;
